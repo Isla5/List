db1dec3ab57d32e485d37b11da8103112c35e6d1
Gohar <aria.g@mail.ru>
2017-03-20 03:20:25 +0400

    adding styling and second column

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 
-class App extends React.Component {
+class App extends Component {
     constructor() {
         super();
         this.pChangeHandler = this.pChangeHandler.bind(this);
         this.state = {
-            pros: ['']
+            pros: [
+                'Its really tasty!',
+                'Its really tasty!',
+                'Its really tasty!',
+                'Its really tasty!',
+                'Its really tasty!',
+                ' '
+            ],
+            cons: ['Makes me fat', 'Too expensive', ' ']
         }
     }
     pChangeHandler = (e) => {
@@ -22,20 +30,89 @@ class App extends React.Component {
             this.setState({pros})
         }
     }
+    cChangeHandler = (e) => {
+        let cons = this.state.cons;
+        let num = e.target.id;
+        this.state.cons[num] = e.target.value;
+        if (cons[num] === '') {
+            cons.splice(num, 1);
+            this.setState({cons});
+        } else if (cons[num] == cons[cons.length - 1]) {
+            this.setState({cons});
+            cons.push('');
+        } else {
+            this.setState({cons})
+        }
+    }
     render() {
+        const main = {
+            display: 'flex',
+            flexDirection: 'column',
+            margin: '139px auto',
+            width: '900px',
+            height: '800px',
+            border: '1px solid grey'
+        }
+        const header = {
+            margin: '0',
+            paddingTop: '20px',
+            paddingBottom: '20px',
+            textAlign: 'center',
+            fontSize: '35.5px',
+            backgroundColor: 'grey',
+            color: 'white'
+        }
+        const twocols = {
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center'
+        }
+        const titles = {
+            textAlign: 'center',
+            fontSize: '30px',
+            color: 'grey'
+        }
+        const pstyle = {
+            height: '715px',
+            border: '2px solid grey'
+        }
+        const listyle = {
+            fontSize: '30px'
+        }
+        const inpStyle = {
+            width: '402px',
+            fontSize: '30px'
+        }
         return (
-            <div>
-                <div>
-                    <div>pros</div>
-                    <ul>
-                        {this.state.pros.map((pro, index) => {
-                            return (
-                                <li>
-                                    <input id={index} value={this.state.pros[index]} onChange={this.pChangeHandler}></input>
-                                </li>
-                            )
-                        })}
-                    </ul>
+            <div style={main}>
+                <h1 style={header}>
+                    Should I eat at McDonalds?
+                </h1>
+                <div style={twocols}>
+                    <div style={pstyle}>
+                        <div style={titles}>PROS</div>
+                        <ol>
+                            {this.state.pros.map((pros, index) => {
+                                return (
+                                    <li style={listyle}>
+                                        <input id={index} style={inpStyle} value={this.state.pros[index]} onChange={this.pChangeHandler}></input>
+                                    </li>
+                                )
+                            })}
+                        </ol>
+                    </div>
+                    <div style={pstyle}>
+                        <div style={titles}>CONS</div>
+                        <ol>
+                            {this.state.cons.map((cons, index) => {
+                                return (
+                                    <li style={listyle}>
+                                        <input id={index} style ={inpStyle} value={this.state.cons[index]} onChange={this.cChangeHandler}></input>
+                                    </li>
+                                )
+                            })}
+                        </ol>
+                    </div>
                 </div>
             </div>
         )
